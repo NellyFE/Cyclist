eb5de9757a2fb80c4a63420121d7fb9449deacb8
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2026-03-08 18:19:11 +0100

    navbar items

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,27 @@
 export const Nav = () => {
     return(
-        <nav>
+        <nav className="flex p-6 mx-auto  ">
 
             {/* logo */}
             <div className="logo">
-                <img src="/cyclistogo.svg" alt="logo" />
-             
-
+                <img src="images/cyclistlogo.svg" alt="logo" className="w-[48px] h-[59px]"/>
+            
             </div>
 
-            <div className="navitems">
+            <div className="navitems flex justify-between items-center w-full">
 
                 {/* the first nav items */}
-                <div className="navside1">
-
+                <div className="navlinks flex mx-6 py-2 px-4 gap-6 bg-[#F5F5F5] rounded-full">
+                    <a href="">How it Works</a>
+                    <a href="">Features</a>
+                    <a href="">Testimonials</a>
+                    <a href="">FAQ</a>
                 </div>
 
 
                 {/* second navitem where download is  */}
-                <div className="navside2">
+                <div className="downloadbtn flex bg-[#D6D6D6] opacity-[60%] py-2 px-4 rounded-full">
+                    <a href="">Download App</a>
 
                 </div>
             </div>
