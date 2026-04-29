78ba168da8d66298c7ededc9ef293323097bcf75
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2026-04-29 17:06:31 +0100

    floating texts

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,8 @@
 export const Hero = () => {
     return (
-        <section className="border py-6">
+
+        <section className="border h-svh py-6">
+          
             {/* users ellipse */}
             <div className="flex justify-center items-center bg-[#FFFCF1] h-[52px] w-1/3 mx-auto rounded-full opacity-[100%]">
                 
@@ -23,6 +25,68 @@ export const Hero = () => {
                 </div>
 
                 </div>
+
+                {/* Phone Mockup section */}
+                <div className="relative bg-gray-50 py-16 px-4 overflow-hidden">
+    
+    <div className="max-w-6xl mx-auto relative z-10">
+        {/* Phone mockups container */}
+        <div className="flex justify-center items-center relative">
+            
+            {/* Left phone with all elements */}
+            <div className="relative -mr-8 ">
+                <img 
+                    src="images/left-phone.svg" 
+                    alt="Rider app" 
+                    className="w-auto h-auto drop-shadow-2xl"
+                />
+            </div>
+
+            {/* Right phone with all elements */}
+            <div className="relative mt-8 -ml-32 ">
+                <img 
+                    src="images/right-phone.svg" 
+                    alt="Driver app" 
+                    className="w-auto h-auto drop-shadow-2xl"
+                />
+            </div>
+        </div>
+        
+        {/* Floating textboxes and icons */}
+        <div className="absolute inset-0 pointer-events-none">
+            <img 
+                src="images/left-floatingtextbox.svg" 
+                alt="" 
+                className="absolute animate-float"
+            />
+            <img 
+                src="images/left-floatingicon.svg" 
+                alt="" 
+                className="absolute animate-float"
+            />
+            <img 
+                src="images/right-floatingtextbox.svg" 
+                alt="" 
+                className="absolute animate-float-delayed"
+            />
+            <img 
+                src="images/right-floatingicon.svg" 
+                alt="" 
+                className="absolute animate-bounce-slow"
+            />
+        </div>
+    </div>
+
+     {/* Background ellipse at bottom */}
+    <div className="absolute -bottom-20 left-1/2 -translate-x-1/2 w-full h-48">
+        <img 
+            src="images/Ellipse1090.svg" 
+            alt="" 
+            className="w-full  object-cover"
+        />
+    </div>
+</div>
+                
 
                 
            
