0108e2fd1bb9d8173a9159a61ae8d7385ec2f2d8
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2026-05-04 17:03:34 +0100

    floating textboxes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,10 @@
 export const Hero = () => {
     return (
 
-        <section className="border h-svh py-6">
+        <section className="h-fit py-6">
           
             {/* users ellipse */}
-            <div className="flex justify-center items-center bg-[#FFFCF1] h-[52px] w-1/3 mx-auto rounded-full opacity-[100%]">
+            <div className="flex justify-center items-center h-[52px] w-1/3 mx-auto rounded-full opacity-[100%]">
                 
                 <img src="images/Ellipse23.svg" alt="logo" className="w-[40px] h-[40px] "/>
                 <img src="images/Ellipse24.svg" alt="logo" className="w-[40px] h-[40px] -ml-5"/>
@@ -27,64 +27,73 @@ export const Hero = () => {
                 </div>
 
                 {/* Phone Mockup section */}
-                <div className="relative bg-gray-50 py-16 px-4 overflow-hidden">
+                <div className="relative py-16 px-4 overflow-hidden">
     
     <div className="max-w-6xl mx-auto relative z-10">
         {/* Phone mockups container */}
         <div className="flex justify-center items-center relative">
             
             {/* Left phone with all elements */}
-            <div className="relative -mr-8 ">
-                <img 
-                    src="images/left-phone.svg" 
-                    alt="Rider app" 
-                    className="w-auto h-auto drop-shadow-2xl"
-                />
-            </div>
+<div className="relative -mr-8 z-12">
+    <img 
+        src="images/left-phone.svg" 
+        alt="Rider app" 
+        className="w-auto h-auto"
+    />
+</div>
 
-            {/* Right phone with all elements */}
-            <div className="relative mt-8 -ml-32 ">
-                <img 
-                    src="images/right-phone.svg" 
-                    alt="Driver app" 
-                    className="w-auto h-auto drop-shadow-2xl"
-                />
-            </div>
+{/* Right phone with all elements */}
+<div className="relative mt-30 -ml-38 z-10">
+    <img 
+        src="images/right-phone.svg" 
+        alt="Driver app"
+        className="w-auto h-auto"
+    />
+</div>
         </div>
         
         {/* Floating textboxes and icons */}
-        <div className="absolute inset-0 pointer-events-none">
-            <img 
-                src="images/left-floatingtextbox.svg" 
-                alt="" 
-                className="absolute animate-float"
-            />
-            <img 
-                src="images/left-floatingicon.svg" 
-                alt="" 
-                className="absolute animate-float"
-            />
-            <img 
-                src="images/right-floatingtextbox.svg" 
-                alt="" 
-                className="absolute animate-float-delayed"
-            />
-            <img 
-                src="images/right-floatingicon.svg" 
-                alt="" 
-                className="absolute animate-bounce-slow"
-            />
-        </div>
-    </div>
+<div className="absolute inset-0 pointer-events-none z-20">
+    {/* Left floating textbox (pricing card "₦2,850") - top left of left phone */}
+    <img 
+        src="images/left-floatingtextbox.svg" 
+        alt="" 
+        className="absolute animate-float top-[50%] -left-[3%]"
+    />
+    
+    {/* Left floating icon (rider arriving indicator) - bottom center of left phone */}
+    <img 
+        src="images/left-floatingicon.svg"
+        alt="" 
+        className="absolute top-[3%] left-[8%]"
+    />
+    
+    {/* Right floating textbox (earnings/delivery card) - right side of right phone */}
+    <img 
+        src="images/right-floatingtextbox.svg"
+        alt="" 
+        className="absolute animate-float-delayed top-[70%] right-[1%]"
+    />
+    
+    {/* Right floating icon (decorative elements at top right) */}
+    <img 
+        src="images/right-floatingicon.svg" 
+        alt="" 
+        className="absolute top-[25%] right-[6.5%]"
+    />
+</div>
 
-     {/* Background ellipse at bottom */}
-    <div className="absolute -bottom-20 left-1/2 -translate-x-1/2 w-full h-48">
+         {/* Background ellipse at bottom */}
+    <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-full z-2">
         <img 
             src="images/Ellipse1090.svg" 
             alt="" 
             className="w-full  object-cover"
         />
     </div>
+    </div>
+
+    
 </div>
                 
 
